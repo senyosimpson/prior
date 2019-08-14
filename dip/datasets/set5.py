0005a05b09a06ff49bac5cb557206c2cec375a67
Senyo <simpsonsenyo@gmail.com>
2019-08-14 23:38:48 +0200

    updated for this use case

--- dip/datasets/set5.py
+++ dip/datasets/set5.py
@@ -14,23 +14,18 @@ class Set5(Dataset):
 
     def build_dataset(self, root):
         image_paths = glob(root)
-        lr_images = sorted([path for path in image_paths if 'LR' in path])
-        hr_images = sorted([path for path in image_paths if path not in lr_images])
-        dataset = list(zip(lr_images, hr_images))
+        dataset = sorted([path for path in image_paths if 'LR' in path])
         return dataset
 
     def __getitem__(self, idx):
-        lres_img_path, hres_img_path = self.dataset[idx]
+        lres_img_path = self.dataset[idx]
         lres_img = Image.open(lres_img_path)
-        hres_img = Image.open(hres_img_path)
         if self.cspace == 'ycbcr':
             lres_img = lres_img.convert('YCbCr')
-            hres_img = hres_img.convert('YCbCr')
-        sample = (lres_img, hres_img)
 
         if self.transform:
-            sample = self.transform(sample)
-        return sample
+            lres_img = self.transform(lres_img)
+        return lres_img
         
     def __len__(self):
         return len(self.dataset)
