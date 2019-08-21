717d45104565aa314bfad5ab7702ebd128082b01
Senyo <simpsonsenyo@gmail.com>
2019-08-21 22:41:26 +0200

    scripts for running jobs on paperspace

---
+++ resolve.sh
@@ -0,0 +1,2 @@
+python3 -m dip.resolve \
+--config dip/config.json
---
+++ submit_job.sh
@@ -0,0 +1,7 @@
+gradient jobs create \
+--name "$0" \
+--container ${CONTAINER_NAME} \
+--registryUsername ${DOCKERHUB_USERNAME} \
+--registryPassword ${DOCKERHUB_PASSWORD} \
+--machineType "P5000" \
+--command "./resolve.sh"
