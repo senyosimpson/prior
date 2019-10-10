5d66e38b21451594deee76b927d3126b9ff56ac8
Senyo <simpsonsenyo@gmail.com>
2019-10-10 21:07:10 +0200

    imresize on gpu

--- dip/utils.py
+++ dip/utils.py
@@ -1,8 +1,14 @@
 """ Code to resize images taken from [BasicSR](https://github.com/xinntao/BasicSR/blob/master/codes/data/util.py)
 Implementation recreates the matlab kernel version so that comparisons can be made with other methods
 """
+import os
 import torch
+import torch.nn as nn
 import math
+import numpy as np
+from glob import glob
+from PIL import Image
+
 
 def cubic(x):
     absx = torch.abs(x)
@@ -13,13 +19,13 @@ def cubic(x):
             (absx > 1) * (absx <= 2)).type_as(absx))
 
 
-def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width, antialiasing):
+def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width, antialiasing, device):
     if (scale < 1) and (antialiasing):
         # Use a modified kernel to simultaneously interpolate and antialias- larger kernel width
         kernel_width = kernel_width / scale
 
     # Output-space coordinates
-    x = torch.linspace(1, out_length, out_length)
+    x = torch.linspace(1, out_length, out_length).to(device)
 
     # Input-space coordinates. Calculate the inverse mapping such that 0.5
     # in output space maps to 0.5 in input space, and 0.5+scale in output
@@ -27,7 +33,7 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
     u = x / scale + 0.5 * (1 - 1 / scale)
 
     # What is the left-most pixel that can be involved in the computation?
-    left = torch.floor(u - kernel_width / 2)
+    left = torch.floor(u - kernel_width / 2).to(device)
 
     # What is the maximum number of pixels that can be involved in the
     # computation?  Note: it's OK to use an extra pixel here; if the
@@ -37,7 +43,7 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
 
     # The indices of the input pixels involved in computing the k-th output
     # pixel are in row k of the indices matrix.
-    indices = left.view(out_length, 1).expand(out_length, P) + torch.linspace(0, P - 1, P).view(
+    indices = left.view(out_length, 1).expand(out_length, P) + torch.linspace(0, P - 1, P).to(device).view(
         1, P).expand(out_length, P)
 
     # The weights used to compute the k-th output pixel are in row k of the
@@ -68,7 +74,7 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
     return weights, indices, int(sym_len_s), int(sym_len_e)
 
 
-def imresize(img, scale, antialiasing=True):
+def imresize(img, scale, device, antialiasing=True):
     # Now the scale should be the same for H and W
     # input: img: CHW RGB [0,1]
     # output: CHW RGB [0,1] w/o round
@@ -85,25 +91,25 @@ def imresize(img, scale, antialiasing=True):
 
     # get weights and indices
     weights_H, indices_H, sym_len_Hs, sym_len_He = calculate_weights_indices(
-        in_H, out_H, scale, kernel, kernel_width, antialiasing)
+        in_H, out_H, scale, kernel, kernel_width, antialiasing, device)
     weights_W, indices_W, sym_len_Ws, sym_len_We = calculate_weights_indices(
-        in_W, out_W, scale, kernel, kernel_width, antialiasing)
+        in_W, out_W, scale, kernel, kernel_width, antialiasing, device)
     # process H dimension
     # symmetric copying
-    img_aug = torch.FloatTensor(in_C, in_H + sym_len_Hs + sym_len_He, in_W)
+    img_aug = torch.FloatTensor(in_C, in_H + sym_len_Hs + sym_len_He, in_W).to(device)
     img_aug.narrow(1, sym_len_Hs, in_H).copy_(img)
 
     sym_patch = img[:, :sym_len_Hs, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long().to(device)
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
     img_aug.narrow(1, 0, sym_len_Hs).copy_(sym_patch_inv)
 
     sym_patch = img[:, -sym_len_He:, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long().to(device)
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
     img_aug.narrow(1, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
 
-    out_1 = torch.FloatTensor(in_C, out_H, in_W)
+    out_1 = torch.FloatTensor(in_C, out_H, in_W).to(device)
     kernel_width = weights_H.size(1)
     for i in range(out_H):
         idx = int(indices_H[i][0])
@@ -113,20 +119,20 @@ def imresize(img, scale, antialiasing=True):
 
     # process W dimension
     # symmetric copying
-    out_1_aug = torch.FloatTensor(in_C, out_H, in_W + sym_len_Ws + sym_len_We)
+    out_1_aug = torch.FloatTensor(in_C, out_H, in_W + sym_len_Ws + sym_len_We).to(device)
     out_1_aug.narrow(2, sym_len_Ws, in_W).copy_(out_1)
 
     sym_patch = out_1[:, :, :sym_len_Ws]
-    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long()
+    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long().to(device)
     sym_patch_inv = sym_patch.index_select(2, inv_idx)
     out_1_aug.narrow(2, 0, sym_len_Ws).copy_(sym_patch_inv)
 
     sym_patch = out_1[:, :, -sym_len_We:]
-    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long()
+    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long().to(device)
     sym_patch_inv = sym_patch.index_select(2, inv_idx)
     out_1_aug.narrow(2, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
 
-    out_2 = torch.FloatTensor(in_C, out_H, out_W)
+    out_2 = torch.FloatTensor(in_C, out_H, out_W).to(device)
     kernel_width = weights_W.size(1)
     for i in range(out_W):
         idx = int(indices_W[i][0])
@@ -135,3 +141,150 @@ def imresize(img, scale, antialiasing=True):
         out_2[2, :, i] = out_1_aug[2, :, idx:idx + kernel_width].mv(weights_W[i])
 
     return out_2
+
+
+class Downsampler(nn.Module):
+    '''
+        http://www.realitypixels.com/turk/computergraphics/ResamplingFilters.pdf
+    '''
+    def __init__(self, n_planes, factor, kernel_type, phase=0, kernel_width=None, support=None, sigma=None, preserve_size=False):
+        super(Downsampler, self).__init__()
+        
+        assert phase in [0, 0.5], 'phase should be 0 or 0.5'
+
+        if kernel_type == 'lanczos2':
+            support = 2
+            kernel_width = 4 * factor + 1
+            kernel_type_ = 'lanczos'
+
+        elif kernel_type == 'lanczos3':
+            support = 3
+            kernel_width = 6 * factor + 1
+            kernel_type_ = 'lanczos'
+
+        elif kernel_type == 'gauss12':
+            kernel_width = 7
+            sigma = 1/2
+            kernel_type_ = 'gauss'
+
+        elif kernel_type == 'gauss1sq2':
+            kernel_width = 9
+            sigma = 1./np.sqrt(2)
+            kernel_type_ = 'gauss'
+
+        elif kernel_type in ['lanczos', 'gauss', 'box']:
+            kernel_type_ = kernel_type
+
+        else:
+            assert False, 'wrong name kernel'
+            
+        # note that `kernel width` will be different to actual size for phase = 1/2
+        self.kernel = get_kernel(factor, kernel_type_, phase, kernel_width, support=support, sigma=sigma)
+        
+        downsampler = nn.Conv2d(n_planes, n_planes, kernel_size=self.kernel.shape, stride=factor, padding=0)
+        downsampler.weight.data[:] = 0
+        downsampler.bias.data[:] = 0
+
+        kernel_torch = torch.from_numpy(self.kernel)
+        for i in range(n_planes):
+            downsampler.weight.data[i, i] = kernel_torch       
+
+        self.downsampler_ = downsampler
+
+        if preserve_size:
+
+            if  self.kernel.shape[0] % 2 == 1: 
+                pad = int((self.kernel.shape[0] - 1) / 2.)
+            else:
+                pad = int((self.kernel.shape[0] - factor) / 2.)
+                
+            self.padding = nn.ReplicationPad2d(pad)
+        
+        self.preserve_size = preserve_size
+        
+    def forward(self, input):
+        if self.preserve_size:
+            x = self.padding(input)
+        else:
+            x = input
+        self.x = x
+        return self.downsampler_(x)
+
+
+def get_kernel(factor, kernel_type, phase, kernel_width, support=None, sigma=None):
+    assert kernel_type in ['lanczos', 'gauss', 'box']
+    
+    # factor  = float(factor)
+    if phase == 0.5 and kernel_type != 'box': 
+        kernel = np.zeros([kernel_width - 1, kernel_width - 1])
+    else:
+        kernel = np.zeros([kernel_width, kernel_width])
+    
+    if kernel_type == 'box':
+        assert phase == 0.5, 'Box filter is always half-phased'
+        kernel[:] = 1./(kernel_width * kernel_width)
+        
+    elif kernel_type == 'gauss': 
+        assert sigma, 'sigma is not specified'
+        assert phase != 0.5, 'phase 1/2 for gauss not implemented'
+        
+        center = (kernel_width + 1.)/2.
+        print(center, kernel_width)
+        sigma_sq =  sigma * sigma
+        
+        for i in range(1, kernel.shape[0] + 1):
+            for j in range(1, kernel.shape[1] + 1):
+                di = (i - center)/2.
+                dj = (j - center)/2.
+                kernel[i - 1][j - 1] = np.exp(-(di * di + dj * dj)/(2 * sigma_sq))
+                kernel[i - 1][j - 1] = kernel[i - 1][j - 1]/(2. * np.pi * sigma_sq)
+    elif kernel_type == 'lanczos': 
+        assert support, 'support is not specified'
+        center = (kernel_width + 1) / 2.
+
+        for i in range(1, kernel.shape[0] + 1):
+            for j in range(1, kernel.shape[1] + 1):
+                
+                if phase == 0.5:
+                    di = abs(i + 0.5 - center) / factor  
+                    dj = abs(j + 0.5 - center) / factor 
+                else:
+                    di = abs(i - center) / factor
+                    dj = abs(j - center) / factor
+                
+                
+                pi_sq = np.pi * np.pi
+
+                val = 1
+                if di != 0:
+                    val = val * support * np.sin(np.pi * di) * np.sin(np.pi * di / support)
+                    val = val / (np.pi * np.pi * di * di)
+                
+                if dj != 0:
+                    val = val * support * np.sin(np.pi * dj) * np.sin(np.pi * dj / support)
+                    val = val / (np.pi * np.pi * dj * dj)
+                
+                kernel[i - 1][j - 1] = val
+    else:
+        assert False, 'wrong method name'
+    
+    kernel /= kernel.sum()
+    return kernel
+
+
+def make_image(image):
+    i = image[0,:,:,:]
+    i = i.cpu().numpy().transpose((1,2,0))
+    return i
+
+
+def make_gif(root):
+    frames = []
+    path = os.path.join(root, '*.jpg')
+    paths = glob(path)
+    for p in paths:
+        image = Image.open(p)
+        frames.append(image)
+
+    # Save into a GIF file that loops forever
+    frames[0].save('moving_ball.gif', format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
